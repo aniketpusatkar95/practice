cb2267dc797cfa43591bc879ead546ec8a3ad97e
D8_62763_AniketPusatkar <dsaniketpusatkar@gmail.com>
2022-06-21 23:28:02 +0530

    ajali 1st commit

--- page1.js
+++ page1.js
@@ -18,3 +18,25 @@ console.log('virat and anushka  are hsband wife')
 console.log('anushka  suraname is sharma she is  actresss')
 
 console.log('virat and anushka  are hsband wife')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('anjali is dr by brpoffession')
+
+console.log('she 1st time meet sachine when she was 25')
+
+console.log('sachine was young cricker that time')
