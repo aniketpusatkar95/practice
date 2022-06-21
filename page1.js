ffd15a6fbd515b44d1177542532d1df8cad706c3
D8_62763_AniketPusatkar <dsaniketpusatkar@gmail.com>
2022-06-21 23:22:51 +0530

    sachinn commit 1

--- page1.js
+++ page1.js
@@ -18,3 +18,13 @@ console.log('virat and anushka  are hsband wife')
 console.log('anushka  suraname is sharma she is  actresss')
 
 console.log('virat and anushka  are hsband wife')
+
+
+
+
+
+console.log('sachin is  god of cricket')
+
+console.log('he does play cover drive very well')
+
+console.log('sachin is g8 father tooo')
