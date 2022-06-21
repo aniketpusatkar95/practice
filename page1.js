0fcea71fedc891a64873fde1d0df58d05add5aa8
D8_62763_AniketPusatkar <dsaniketpusatkar@gmail.com>
2022-06-21 22:09:36 +0530

    2nd commit of anushka 1st was mistake

--- page1.js
+++ page1.js
@@ -5,6 +5,16 @@ console.log('virats suraname is kohli')
 console.log('virat and anushka  are hsband wife')
 
 
+
+
+console.log('anushka  suraname is sharma she is  actresss')
+
+console.log('virat and anushka  are hsband wife')
+
+console.log('anushka  suraname is sharma she is  actresss')
+
+console.log('virat and anushka  are hsband wife')
+
 console.log('anushka  suraname is sharma she is  actresss')
 
 console.log('virat and anushka  are hsband wife')
